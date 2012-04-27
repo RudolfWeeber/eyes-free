79fde86d54f86b78211273845351f0dd998758b9
alanv@google.com <alanv@google.com@584082c0-ab3a-11dd-9ddb-6f86aeb5eef6>
2012-04-27 22:25:24 +0000

    Update SoundBack to support new TalkBack
    git-svn-id: http://eyes-free.googlecode.com/svn@773 584082c0-ab3a-11dd-9ddb-6f86aeb5eef6

---
+++ trunk/accessibilityServices/soundback/src/com/google/android/marvin/soundback/PackageManagerUtils.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.marvin.soundback;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+/**
+ * Utilities for interacting with the {@link PackageManager}.
+ *
+ * @author alanv@google.com (Alan Viverette)
+ */
+class PackageManagerUtils {
+    /** Invalid version code for a package. */
+    public static final int INVALID_VERSION_CODE = -1;
+
+    /**
+     * @return The package version code or {@link #INVALID_VERSION_CODE} if the
+     *         package does not exist.
+     */
+    public static int getVersionCode(Context context, String packageName) {
+        final PackageInfo packageInfo = getPackageInfo(context, packageName);
+
+        if (packageInfo == null) {
+            return INVALID_VERSION_CODE;
+        }
+
+        return packageInfo.versionCode;
+    }
+
+    private static PackageInfo getPackageInfo(Context context, String packageName) {
+        final PackageManager packageManager = context.getPackageManager();
+
+        try {
+            return packageManager.getPackageInfo(packageName, 0);
+        } catch (NameNotFoundException e) {
+            return null;
+        }
+    }
+}
--- trunk/accessibilityServices/soundback/src/com/google/android/marvin/soundback/SoundBackService.java
+++ trunk/accessibilityServices/soundback/src/com/google/android/marvin/soundback/SoundBackService.java
@@ -18,6 +18,7 @@ package com.google.android.marvin.soundback;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.media.AudioManager;
 import android.media.SoundPool;
 import android.util.Log;
 import android.util.SparseArray;
@@ -29,56 +30,85 @@ import android.view.accessibility.AccessibilityEvent;
  * @author svetoslavganov@google.com (Svetoslav R. Ganov)
  */
 public class SoundBackService extends AccessibilityService {
+    private static final String TALKBACK_PACKAGE = "com.google.android.marvin.talkback";
+
+    /** The minimum version required for SoundBack to disable itself. */
+    private static final int TALKBACK_REQUIRED_VERSION = 42;
 
     private static final String LOG_TAG = "SoundBackService";
 
-    private static final int NUMBER_OF_CHANNELS = 4;
-    private static final int STREAM_MUSIC = 3;
-    private static final float SOUND_EFFECT_VOLUME = 1000.0f;
-
     private static final int NOTIFICATION_TIMEOUT_MILLIS = 80;
+    private static final int NUMBER_OF_CHANNELS = 10;
+    private static final int DEFAULT_STREAM = AudioManager.STREAM_MUSIC;
+    private static final float SOUND_EFFECT_VOLUME = 1.0f;
 
-    private SoundPool mSoundPool = new SoundPool(NUMBER_OF_CHANNELS, STREAM_MUSIC, 0);
-    private SparseArray<Integer> mResourceIdToSoundIdMap = new SparseArray<Integer>();
+    private final SoundPool mSoundPool = new SoundPool(NUMBER_OF_CHANNELS, DEFAULT_STREAM, 0);
+    private final SparseArray<Integer> mResourceIdToSoundIdMap = new SparseArray<Integer>();
     private int currentSoundId;
+
+    /** Whether this service should disable itself. */
+    private boolean mDisabled;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // If TalkBack r42 or higher is installed, this service should be quiet.
+        final int talkBackVersion = PackageManagerUtils.getVersionCode(this, TALKBACK_PACKAGE);
+
+        if (talkBackVersion >= TALKBACK_REQUIRED_VERSION) {
+            mDisabled = true;
+        }
+    }
 
     @Override
     public void onServiceConnected() {
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        if (mDisabled) {
+            return;
+        }
+
+        final AccessibilityServiceInfo info = new AccessibilityServiceInfo();
         info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_AUDIBLE;
         info.notificationTimeout = NOTIFICATION_TIMEOUT_MILLIS;
         info.flags = AccessibilityServiceInfo.DEFAULT;
+
         setServiceInfo(info);
     }
 
     @Override
-    public synchronized void onAccessibilityEvent(AccessibilityEvent event) {
-        if (event == null){
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        if (mDisabled) {
+            return;
+        }
+
+        if (event == null) {
             Log.e(LOG_TAG, "Received null accessibility event.");
             return;
         }
-        int eventType = event.getEventType();
+
+        final int eventType = event.getEventType();
+
         switch (eventType) {
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED : 
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
                 playEarcon(R.raw.open);
                 return;
-            case AccessibilityEvent.TYPE_VIEW_SELECTED : 
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
                 playEarcon(R.raw.select);
                 return;
-            case AccessibilityEvent.TYPE_VIEW_CLICKED : 
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
                 playEarcon(R.raw.button);
                 return;
-            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED :
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
                 playEarcon(R.raw.open);
                 return;
-            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED :
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
                 playEarcon(R.raw.item);
                 return;
-            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED :
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
                 playEarcon(R.raw.working);
                 return;
-            default :
+            default:
                 Log.w(LOG_TAG, "Unknown accessibility event type " + eventType);
         }
     }
@@ -90,13 +120,13 @@ public class SoundBackService extends AccessibilityService {
 
     /**
      * Plays an earcon given its resource id.
-     * 
+     *
      * @param resourceId The resource id of the earcon to be played.
      */
-    private void playEarcon(int resourceId) {
+    private synchronized void playEarcon(int resourceId) {
         Integer soundId = mResourceIdToSoundIdMap.get(resourceId);
         if (soundId == null) {
-            soundId = mSoundPool.load(this, resourceId, 1234567);
+            soundId = mSoundPool.load(this, resourceId, 1);
             mResourceIdToSoundIdMap.put(resourceId, soundId);
         }
         currentSoundId = soundId;
